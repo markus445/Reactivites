063677ebad7ebc46cef2ca2e0869089f86c41f04
Markus Markussen <Markus.Markussen@geodata.no>
2020-03-11 09:48:56 +0100

    First commit

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Domain;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {            
+        }
+
+        public DbSet<Value> Values {get; set;}
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Value>().HasData(
+                    new Value {Id = 1, Name = "Markus1"},
+                    new Value {Id = 2, Name = "Markus2"},
+                    new Value {Id = 3, Name = "Markus3"}
+                );
+        }
+    }
+}
